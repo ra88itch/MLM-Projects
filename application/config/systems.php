fa21900a91149c6f09b887a94c97ebd191025d67
ra88itch <npk@codesmash.net>
2019-01-09 01:48:47 +0700

    TEMPLATE

---
+++ application/config/systems.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/* MANUAL */
+$systems['response'] = array(
+			'method' => '',
+			'errorCode' => '0',
+			'errorDesc' => 'MLM',
+			'isLogin' => TRUE,
+			'root' => 'http://'.$_SERVER['SERVER_NAME'].'/',
+			'title' => 'MLM'
+		);
+
+$systems['maintenance'] = false;
